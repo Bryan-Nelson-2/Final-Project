7c2194b7ee65105d68cd678c501a7635b5143ceb
B <bryan.nelson@stu.ocsb.ca>
2026-01-08 13:29:21 -0500

    Added sonar code

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Bryan
+ * Created on: Jan 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let motor: stepperMotor.Motor
+let distanceToObject: number = 0
+
+//setup
+basic.showString("READY")
+robotbit.MotorRun(robotbit.Motors.M1A, 150)
+
+
+// when button A is pressed, LEFT wheel spins foraward
+input.onButtonPressed(Button.A, function() {
+    robotbit.StepperDual(1000, 1000)
+    basic.pause(100)
+})
+
+
+while (true) {
+    // gets the distance constantly
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showIcon(IconNames.Happy)
+
+    if (distanceToObject < 10) {
+        robotbit.StepperDual(0, 0)
+
+    } else {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+    }
+}
+
+
+//set radio group
+radio.setGroup(41)
