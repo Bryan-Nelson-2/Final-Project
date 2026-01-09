c38410e36b636d211039653c9a023cc46b41fac2
B <bryan.nelson@stu.ocsb.ca>
2026-01-09 10:28:12 -0500

    Added sonar code ang got rid of copy

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@
 
 // variables
 let motor: stepperMotor.Motor
+let distanceToObject: number = 0
 
 //setup
 basic.showString("READY")
@@ -14,6 +15,24 @@ robotbit.MotorRun(robotbit.Motors.M1A, 150)
 
 // if button A is pressed, LEFT wheel spins foraward
 input.onButtonPressed(Button.A, function() {
-    robotbit.StepperDual(360, 360)
+    robotbit.StepperDual(1000000, 1000000)
     basic.pause(100)
 })
+
+while (true) {
+    // gets the distance constantly
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showIcon(IconNames.Happy)
+
+    if (distanceToObject < 10) {
+        radio.sendString("Too Close")
+
+    } else {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+    }
+}
