878eed72acdf69ff5d0b4cbabf53a42bd08cd5f9
B <bryan.nelson@stu.ocsb.ca>
2026-01-12 12:36:38 -0500

    Some way, somehow, we made it.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Bryan
+ * Created by: Kyphong Pham and Alex Bryan
  * Created on: Jan 2026
- * This program ...
+ * This program spins wheels forward when you press A on the other microbit and stops when sonar is with 10cm of an object
 */
 
 // variables
@@ -11,32 +11,28 @@ let distanceToObject: number = 0
 
 //setup
 basic.showString("READY")
-robotbit.MotorRun(robotbit.Motors.M1A, 150)
 radio.setGroup(41)
 
 // When button A is pressed, LEFT wheel spins foraward
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber = 1) {
-        input.onButtonPressed(Button.A, function() {
+    if (receivedNumber == 1) {
         robotbit.StepperDual(1000000, 1000000)
-        basic.pause(100)
+        basic.pause(100)}
 })
 
-while (true) {
-    // gets the distance constantly
-    distanceToObject = sonar.ping(
-        DigitalPin.P1,
-        DigitalPin.P2,
-        PingUnit.Centimeters
+basic.forever(function () {
+// gets the distance constantly
+        distanceToObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
     )
-    basic.showIcon(IconNames.Happy)
 
     if (distanceToObject < 10) {
-        radio.sendString("Too Close")
+        robotbit.StepperDual(0, 0)
 
     } else {
         basic.showIcon(IconNames.Happy)
         basic.pause(500)
     }
-}
-    }})
+})
